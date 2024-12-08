f8ba66208567a9925155b1c1dd689a9ed8a5ea5c
eminivince <communicatti@gmail.com>
2024-12-08 14:04:07 +0100

    provider

--- src/Components/FlexibleStakeComponent.jsx
+++ src/Components/FlexibleStakeComponent.jsx
@@ -67,9 +67,10 @@ const FlexibleStakeComponent = ({ isConnected }) => {
     const initializeProvider = async () => {
       if (isConnected && window.ethereum) {
         try {
-          const web3Provider = new ethers.providers.Web3Provider(
-            window.ethereum
+          const web3Provider = new ethers.JsonRpcProvider(
+            "https://network.ambrosus.io"
           );
+
           setProvider(web3Provider);
           const signerInstance = web3Provider.getSigner();
           setSigner(signerInstance);
