cc98dc9b3cbd1cb50c95450e9bec87a6fefb376c
eminivince <communicatti@gmail.com>
2024-12-08 13:47:27 +0100

    err off

--- src/Components/FixedStakeComponent.jsx
+++ src/Components/FixedStakeComponent.jsx
@@ -534,7 +534,7 @@ const FixedStakeComponent = ({ isConnected }) => {
       }
     } catch (err) {
       console.error("Error during fixed staking:", err);
-      setFixedError(err);
+      setFixedError("An error occurred during fixed staking.");
       setIsFixedStaking(false);
     }
   };
